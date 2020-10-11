528936f1a7860c3e58282d03b0baafa93713a899
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-10-11 18:50:04 +0300

    init

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('main');
+    }
+
+    public function categories()
+    {
+        $categories = Category::get();
+        return view('categories', compact('categories'));
+    }
+
+    public function category($code)
+    {
+        $category = Category::where('code', $code)->first();
+        return view('category', compact('category'));
+    }
+
+    public function product($product = null)
+    {
+        return view('product', ['product' => $product]);
+    }
+}
